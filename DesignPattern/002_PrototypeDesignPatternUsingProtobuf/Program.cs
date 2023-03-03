ef8c73ce5bb29f864860b46101fb4517d79fbaec
Venkatesan Panneerselvam <venkatpselvam1@gmail.com>
2023-03-03 18:31:37 +0530

    prototype design pattern using google's protopuf

---
+++ DesignPattern/002_PrototypeDesignPatternUsingProtobuf/Program.cs
@@ -0,0 +1,64 @@
+﻿using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_PrototypeDesignPatternUsingProtobuf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrototypeDesignPatternWithCopyCat();
+            SerialzlizationDeserializationCopy();
+        }
+        static void SerialzlizationDeserializationCopy()
+        {
+            var prototype = new ComplexModel();
+            prototype.A = 1;
+            prototype.B = 2;
+            prototype.C = 3;
+            prototype.D = 4;
+            prototype.W = "Venkat";
+            prototype.X = "Nandhni";
+            prototype.Y = "Mom";
+            prototype.Z = "Dad";
+            Console.WriteLine("Prototype " + prototype);
+
+            //Type1: Serialize the object to bytestring and deserialize from bytestring
+            var byteString = prototype.ToByteString();
+  
+            var newObj = ComplexModel.Parser.ParseFrom(byteString);
+            newObj.Y = "sagunthala";
+            newObj.Z = "panner";
+            Console.WriteLine("Copycat " + newObj);
+
+            //Type2: Serialize to and deserialize from base64
+            var str = byteString.ToBase64();
+            Console.WriteLine("Protopub " + str);
+            var newObj1 = ComplexModel.Parser.ParseFrom(ByteString.FromBase64(str));
+            newObj1.Y = "sagunthala p";
+            newObj1.Z = "panneer s";
+            Console.WriteLine("Copycat using base64 " + newObj1);
+        }
+        static void PrototypeDesignPatternWithCopyCat()
+        {
+            var prototype = new ComplexModel();
+            prototype.A = 1;
+            prototype.B = 2;
+            prototype.C = 3;
+            prototype.D = 4;
+            prototype.W = "Venkat";
+            prototype.X = "Nandhni";
+            prototype.Y = "Mom";
+            prototype.Z = "Dad";
+            Console.WriteLine("Prototype " + prototype);
+            var newObj = new ComplexModel(prototype);
+            newObj.Y = "sagunthala";
+            newObj.Z = "panner";
+            Console.WriteLine("Copycat " + newObj);
+        }
+    }
+}
---
+++ DesignPattern/002_PrototypeDesignPatternUsingProtobuf/Readme.md
@@ -0,0 +1,16 @@
+﻿# Steps to generate the VenkatModel.cs from protopub
+1. Create new file "VenkatModel.proto"
+2. Define the model defination and namespace
+3. Download the protoc generator executable  https://github.com/protocolbuffers/protobuf/releases/tag/v22.0
+4. Run the following command to generate the model
+```
+protoc -I=$SRC_DIR --csharp_out=$DST_DIR $SRC_DIR/addressbook.proto
+```
+
+e.g:
+```
+C:\Users\VenkatP\Downloads\protoc-22.0-win64\bin\protoc.exe -I="C:\Venkat\Repos\DesignPattern\DesignPattern\002_PrototypeDesignPatternUsingProtobuf" --csharp_out="C:\Venkat\Repos\DesignPattern\DesignPattern\002_PrototypeDesignPatternUsingProtobuf" VenkatModel.proto
+```
+
+For more detials:
+https://protobuf.dev/getting-started/csharptutorial/
