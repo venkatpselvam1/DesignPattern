4e03e466d26f9a97f01a745b45ae473f4a113c36
venkat <venkatpselvam1@gmail.com>
2023-03-04 21:12:48 +0530

    Flyweight Design pattern half

---
+++ DesignPattern/001_FlyWeightDesignPattern/DatabaseProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_FlyWeightDesignPattern
+{
+    internal class DatabaseProvider
+    {
+        Dictionary<int, string> dataBase = new Dictionary<int, string>();
+        public void AddEntry(int id, String  name)
+        {
+            dataBase.Add(id, name);
+        }
+    }
+}
---
+++ DesignPattern/001_FlyWeightDesignPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_FlyWeightDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Flyweight pattern is one of the structural design patterns
+             * as this pattern provides ways to decrease object count
+             * thus improving application required objects structure. 
+             */
+            ExampleFlyWeightDesignPattern();
+        }
+
+        private static void ExampleFlyWeightDesignPattern()
+        {
+            var db = new DatabaseProvider();
+            db.AddEntry(1, "abcd");
+            db.AddEntry(2, "abcd");
+            db.AddEntry(3, "abcd");
+            db.AddEntry(4, "abcd");
+            //in actual SQL DB, for each row for this string entry will cost 4 char = 4 
+        }
+    }
+}
